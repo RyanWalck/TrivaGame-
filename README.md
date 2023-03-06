4ca6585a9250ab29d2d83d5f9ab9c5689af4fbec
RyanWalck <rwalck33@gmail.com>
2023-03-05 19:29:38 -0500

    More done

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # TrivaGame-
-In This Triva Game you will have 5 questions to test ypur knowlegde on a wide verity of things.
-Once loaded up the game please move forward and start anwesring all the questions.
-Try to get them all 5 right to become The Ultimate Triva Warrior .
-If you anwesr 4 correct you will become The Above Average Triva Warrior .
-Anwser 3 correct and you will just become a Average Triva Warrior .
-Anwser only 2 correct and you will become a Confused Triva Warrior .
-Anwser 1 correct and you will become a very Lost Triva Warrior .
-And if you anwser none correct you are a Dumb triva Warrior.
+In This Triva Game you will have 10 questions based on Acient Greek Mythology. Once you have loaded up the link proceded to anwser the questions to the best of your ability. Depending on how many you anwser correct you will receive a special message as well with how many correct anwsers you got! The goal is to anwser all 10 questions correct. Try your best and have fun with this triva game. 
--- main.js
+++ main.js
@@ -14,5 +14,30 @@ function trivia(choice) {
     console.log("wrongs=" + wrongs);
     qs=qs+1
     document.getElementById("q"+qs).style.display="none";
+    if(qs<10){
     document.getElementById("q"+(qs+1)).style.display="block";
+    }
+    else{
+        showresults()
+    }
 }
+
+function showresults(){
+    if (rights<3){
+        document.getElementById("results").innerHTML=("You Got" + rights + "Anwsers Right!<p>You are just a human being :( </p>");
+    }
+    else if (rights < 6){
+        document.getElementById("results").innerHTML=("You Got" + rights + "Anwsers Right!<p> You are on your way to becoming a Greek God!</p>");
+    }
+    else if (rights <9){
+        document.getElementById("results").innerHTML=("You Got" + rights + "Anwsers Right!<p>You are a Demi-God! </p>");
+    }
+    else if(rights =10){
+        document.getElementById("results").innerHTML=("You Got" + rights + "Anwsers Right!<p>Are you sure you are not a Greek God yourself? </p>");
+    }
+
+
+
+        document.getElementById("results").style.display="block";
+}
+
